68a4fea7ca544740dfe076e80ed64c9bd2f95ae6
FTFumetsu <esmieudamien18@gmail.com>
2026-01-28 16:42:22 +0100

    Start Ui Trader

--- Assets/Dynamic/Scripts/Interactables/Trader/TraderInteractable.cs
+++ Assets/Dynamic/Scripts/Interactables/Trader/TraderInteractable.cs
@@ -5,11 +5,13 @@ namespace Interactables.Trader
     public class TraderInteractable : MonoBehaviour, IInteractable
     {
         [SerializeField] private GameObject _tradeUi;
+        [SerializeField] private GameObject _lureTradeUi;
         public void OnInteract()
         {
             if (_tradeUi != null)
             {
                 _tradeUi.SetActive(true);
+                _lureTradeUi.SetActive(false);
             }
         }
     }
---
+++ Assets/Dynamic/Scripts/UI/TraderPanel/SetAllLuresUI.cs
@@ -0,0 +1,60 @@
+using Lure.Data;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.TraderPanel
+{
+    public class SetAllLuresUI : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _luresLayoutGroup;
+        [SerializeField] private GameObject _lureTradeUi;
+
+        [SerializeField] private GameObject _lureUIPrefab;
+        [SerializeField] private LureData[] _lureDatas;
+
+        private readonly List<GameObject> _spawnedItems = new();
+
+        public GameObject LureTradeUi => _lureTradeUi;
+
+        private void OnEnable()
+        {
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            ClearItems();
+
+            if(_luresLayoutGroup == null || _lureUIPrefab == null || _lureDatas.Length == 0)
+            {
+                Debug.LogWarning("SetAllLuresUI: Missing references or no lure data available.");
+                return;
+            }
+
+            foreach (var lureData in _lureDatas)
+            {
+                var prefab = Instantiate(_lureUIPrefab, _luresLayoutGroup);
+                prefab.transform.SetParent(_luresLayoutGroup, false);
+                var item = prefab.GetComponent<Lure.UI.SetupLurePrefabsUi>();
+                if(item != null)
+                {
+                    item.SetupLureUI(lureData, this);
+                }
+                _spawnedItems.Add(prefab);
+            }
+        }
+
+        public void ClearItems()
+        {
+            for (int i = 0; i < _spawnedItems.Count; i++)
+            {
+                if(_spawnedItems[i] != null)
+                {
+                    Destroy(_spawnedItems[i]);
+                }
+            }
+            _spawnedItems.Clear();
+        }
+    }
+}
