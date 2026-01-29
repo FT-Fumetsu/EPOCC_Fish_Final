7ab3ab1ee60194859a2669fc0e121c2f9a3e6d6b
FTFumetsu <esmieudamien18@gmail.com>
2026-01-29 16:27:24 +0100

    Push LittlePoissong

--- Assets/Dynamic/Scripts/Fishs/FishInteractable.cs
+++ Assets/Dynamic/Scripts/Fishs/FishInteractable.cs
@@ -6,9 +6,10 @@ namespace Interactables.Fishs
     [RequireComponent(typeof(Collider))]
     public class FishInteractable : MonoBehaviour, IInteractable
     {
+        [SerializeField, Range(1, 100)] private int _fishCount;
         public void OnInteract()
         {
-            FishsManager.Instance.AddFishs(1);
+            FishsManager.Instance.AddFishs(_fishCount);
             //Destroy(gameObject);
         }
     }
