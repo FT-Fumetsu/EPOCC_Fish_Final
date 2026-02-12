af6edd8416ca01d52b43f3b39d47721e363c1116
FTFumetsu <esmieudamien18@gmail.com>
2026-02-12 12:42:46 +0100

    Save & Load Works (normally)

--- Assets/Dynamic/Scripts/SaveSystem/SaveData.cs
+++ Assets/Dynamic/Scripts/SaveSystem/SaveData.cs
@@ -6,11 +6,39 @@ namespace Save.Data
     [System.Serializable]
     public class SaveData
     {
-        public Vector3 playerPosition;
+        // Utiliser une structure simple pour éviter les problèmes de sérialisation avec UnityEngine.Vector3
+        public SerializableVector3 playerPosition;
 
         public int totalFishCount;
         
         public Dictionary<int, int> countByLure;
         public Dictionary<int, int> countByFish;
+    }
+
+    [System.Serializable]
+    public struct SerializableVector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public SerializableVector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public SerializableVector3(Vector3 v)
+        {
+            x = v.x;
+            y = v.y;
+            z = v.z;
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3(x, y, z);
+        }
     }
 }
