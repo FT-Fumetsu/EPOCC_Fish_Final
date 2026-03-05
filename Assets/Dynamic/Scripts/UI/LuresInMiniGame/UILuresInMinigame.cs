797de26f0890b0560be1d78e48da860f6f886a97
FTFumetsu <esmieudamien18@gmail.com>
2026-03-05 15:02:19 +0100

    Add Falback for Lure Used

--- Assets/Dynamic/Scripts/UI/LuresInMiniGame/UILuresInMinigame.cs
+++ Assets/Dynamic/Scripts/UI/LuresInMiniGame/UILuresInMinigame.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 using Lure.Data;
 using Manager.Lure;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace UI.Lures.Buttons
@@ -21,8 +24,16 @@ namespace UI.Lures
     using Buttons;
    public class UILuresInMinigame : MonoBehaviour
    {
-       [SerializeField] private GameObject luresButton;
-       [SerializeField] private List<LureButtonEntry> lureButtons = new();
+       [SerializeField] private GameObject _luresButton;
+       [SerializeField] private List<LureButtonEntry> _lureButtons = new();
+       [SerializeField] private GameObject _usedLureText;
+       
+       [Header("SFX")]
+       [SerializeField] private AudioClip _lureSfx;
+       [SerializeField, Range(0f, 1f)] private float _sfxVolume = 1f;
+       [SerializeField] private AudioMixerGroup _sfxGroup;
+       
+       private Coroutine _usedLureCoroutine;
        
        private void Start()
        {
@@ -32,7 +43,7 @@ namespace UI.Lures
        
        private void SetupButtons()
        {
-           foreach (var entry in lureButtons)
+           foreach (var entry in _lureButtons)
            {
                if (entry == null || entry.button == null)
                    continue;
@@ -59,7 +70,7 @@ namespace UI.Lures
    
            if (lureManager != null && lure != null)
            {
-               used = lureManager.BoolUseLure(lure); // appel direct, compile-time checked
+               used = lureManager.BoolUseLure(lure);
            }
    
            if (used)
@@ -78,34 +89,54 @@ namespace UI.Lures
                    spawner.SetActiveLure(lure);
                }
                
-               
                CheckAllLures(true);
                CheckLureButton(true);
+               
+               if (_lureSfx != null)
+               {
+                   var pos = Camera.main != null ? Camera.main.transform.position : transform.position;
+                   PlaySFX.PlayClipAtPosition(_lureSfx, pos, Mathf.Clamp01(_sfxVolume), true, true, _sfxGroup);
+               }
+
+               if (_usedLureText == null) 
+                   return;
+               
+               if (_usedLureCoroutine != null)
+                   StopCoroutine(_usedLureCoroutine);
+               _usedLureCoroutine = StartCoroutine(ShowUsedLureTextRealtime(2f));
            }
+       }
+
+       private IEnumerator ShowUsedLureTextRealtime(float duration)
+       {
+           _usedLureText.SetActive(true);
+           yield return new WaitForSecondsRealtime(duration);
+           _usedLureText.SetActive(false);
+           _usedLureCoroutine = null;
        }
    
        private void CheckLureButton(bool lureUsed)
        {
            if (!LureManager.Instance.HasAnyLure() || lureUsed)
            {
-               luresButton.SetActive(false);
+               _luresButton.SetActive(false);
                return;
            }
-           luresButton.SetActive(true);
+           _luresButton.SetActive(true);
        }
    
        private void CheckAllLures(bool lureUsed)
        {
            if (lureUsed)
            {
-               foreach (var lureButtonItem in lureButtons)
+               foreach (var lureButtonItem in _lureButtons)
                {
                    lureButtonItem.button.SetActive(false);
                }
 
                return;
            }
-           foreach (var lureButtonItem in lureButtons)
+           foreach (var lureButtonItem in _lureButtons)
            {
                if (lureButtonItem == null || lureButtonItem.button == null)
                    continue;
@@ -120,4 +151,3 @@ namespace UI.Lures
        }
    } 
 }
-
